c3658b5aab9f387abd3c81a5a6e4d5745fc47d35
ananya <ananya884.sharma@gmail.com>
2025-11-01 20:08:47 +0530

    Force clean dependency install

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
 const dotenv = require("dotenv");
+const router = require('./router');
 
 dotenv.config();
 
